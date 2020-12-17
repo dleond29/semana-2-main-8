f25081d825b9b7374b82deee58448c2aee39da69
DiegoLeon <diegoleon.77@hotmail.com>
2020-12-17 13:52:58 -0500

    Primera versión página web

---
+++ src/App.vue
@@ -0,0 +1,83 @@
+<template>
+  <div id="app">
+
+    <top-banner></top-banner>
+    <services></services>
+    <div class="container-fluid">
+          <section-api></section-api>
+    </div>
+      
+      <div class="container-fluid">
+        <div class="row justify-content-center mb-5">
+          <div class="col mt-5" v-for="(item, index) of team" :key="index">
+            <team-card v-bind:member="item"></team-card>
+          </div>
+        </div>
+      </div>
+
+    <page-footer></page-footer>
+    <h1>{{ title }}</h1>
+  </div>
+</template>
+
+<script>
+import PageFooter from "./components/PageFooter.vue";
+import TeamCard from "./components/TeamCard.vue";
+import SectionApi from "./components/SectionApi.vue";
+import TopBanner from './components/TopBanner.vue';
+import Services from './components/Services'
+
+export default {
+  name: "App",
+  components: {
+    PageFooter,
+    TeamCard,
+    SectionApi,
+    TopBanner,
+    Services,
+  },
+  data() {
+    return {
+      title: "Vivo dentro de VUE",
+      team: [
+        {
+          codigo: 1,
+          nombre: "Diego León",
+          descripcion: "1. Miembro equipo 8.",
+          rol: "Desarrollador backend",
+          imagen: "images/diegoL.png",
+        },
+        {
+          codigo: 2,
+          nombre: "Edwar Carrillo",
+          descripcion: "2. Miembro equipo 8.",
+          rol: "Desarrollador fronted",
+          imagen: "images/edwarC.png",
+        },
+        {
+          codigo: 3,
+          nombre: "Jahir Rodríguez",
+          descripcion: "3. Miembro equipo 8.",
+          rol: "Desarrollador full stack",
+          imagen: "images/jahirR.png",
+        },
+        {
+          codigo: 4,
+          nombre: "Andrés González",
+          descripcion: "4. Miembro equipo 8.",
+          rol: "Arquitecto de software",
+          imagen: "images/andresG.png",
+        },
+        {
+          codigo: 5,
+          nombre: "Elmer Novoa",
+          descripcion: "5. Miembro equipo 8.",
+          rol: "Desarrollador junior",
+          imagen: "images/elmerN.png",
+        },
+      ],
+    };
+  },
+};
+</script>
+
---
+++ src/components/Services.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="container-fluid">
+        <div class="row justify-content-center mb-5">
+            <div class="col-sm-6 col-xs-12 col-lg-4 mt-5">
+                <div class="card text-center">
+                    <div class="card-body">
+                        <img src="assets/top.png" class="card-img-top" alt="Top">
+                        <h5 class="card-title">TOP PICKS</h5>
+                        <a href="https://www.imdb.com/what-to-watch/top-picks/?ref_=watch_wls_tab" class="btn btn-primary" target="_blank">Info</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-xs-12 col-lg-4 mt-5">  
+                <div class="card text-center">             
+                    <div class="card-body">
+                        <img src="assets/popular.png" class="card-img-top" alt="Popular">
+                        <h5 class="card-title">MOST POPULAR</h5>
+                        <a href="https://www.imdb.com/what-to-watch/popular/?ref_=watch_fanfav_tab" class="btn btn-primary" target="_blank">Info</a>
+                    </div>
+                </div>  
+            </div>  
+            <div class="col-sm-6 col-xs-12 col-lg-4 mt-5">
+                <div class="card text-center"> 
+                    <div class="card-body">
+                        <img src="assets/fans.png" class="card-img-top" alt="Fans">
+                        <h5 class="card-title">FAN FAVORITES</h5>
+                        <a href="https://www.imdb.com/what-to-watch/fan-favorites/?ref_=watch_tpks_tab" class="btn btn-primary" target="_blank">Info</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "Services",
+};
+</script>
+
+<style scoped>
+</style>>
